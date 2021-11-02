0e035eb432fd58bd5094304bbd008f8fd1efcc56
Maxime <maxime.siegl@laplateforme.io>
2021-11-02 11:52:43 +0100

    commit de pause repas / job jusqu'au 17 check

---
+++ jour01/job11/main.py
@@ -0,0 +1,9 @@
+def add(x, y):
+    return x + y
+
+
+print(add(5, 6))
+print(add(12, 6))
+print(add(4, 8))
+print(add(8, -4))
+print(add(7, 1))
---
+++ jour01/job17/main.py
@@ -0,0 +1,10 @@
+num1 = int(input("Enter First number : "))
+num2 = int(input("Enter Second number : "))
+num3 = int(input("Enter Third number : "))
+num4 = int(input("Enter Fourth number : "))
+num5 = int(input("Enter Fith number : "))
+
+listNum = [num1, num2, num3, num4, num5]
+
+
+print(sorted(listNum))
